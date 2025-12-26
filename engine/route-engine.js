239da371da5ef86bcb897db35cb94b2a2622c36a
jodycboyd-create <jodycboyd@gmail.com>
2025-12-26 17:21:54 -0330

    Update route-engine.js

--- engine/route-engine.js
+++ engine/route-engine.js
@@ -1,1 +1,28 @@
-{}
+// route-engine.js - The [weong-route] geographic processor
+export class RouteEngine {
+    constructor(communities) {
+        this.communities = communities;
+        this.islandData = new Map();
+        this.initializeEngine();
+    }
+
+    initializeEngine() {
+        // Locks in the Newfoundland dataset logic
+        this.communities.forEach(loc => {
+            this.islandData.set(loc.id || loc.name, {
+                ...loc,
+                region: loc.region || "Island-Wide",
+                locked: true
+            });
+        });
+        console.log("Route Engine: Newfoundland Dataset Locked.");
+    }
+
+    getCommunityData(id) {
+        return this.islandData.get(id);
+    }
+
+    getAllCommunities() {
+        return Array.from(this.islandData.values());
+    }
+}
