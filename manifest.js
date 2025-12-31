d07ae9454d8bc325c94c7bb9a7ab40577d803701
jodycboyd-create <jodycboyd@gmail.com>
2025-12-31 14:25:48 -0330

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,22 +1,11 @@
-{
-  "manifest_version": 3,
-  "name": "[weong-route] + [weong-bulletin] L3",
-  "version": "1.3.0",
-  "content_scripts": [
-    {
-      "matches": ["<all_urls>"],
-      "js": [
+/** * Project: [weong-bulletin] + [weong-route]
+ * Manifest Registry (L3 Unified)
+ */
+const WebManifest = {
+    "version": "1.3.0",
+    "modules": [
         "route-engine.js",
         "velocity-widget.js",
         "weather-engine.js"
-      ],
-      "run_at": "document_end"
-    }
-  ],
-  "web_accessible_resources": [
-    {
-      "resources": ["data/nl/communities.json"],
-      "matches": ["<all_urls>"]
-    }
-  ]
-}
+    ]
+};
