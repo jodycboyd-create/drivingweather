7a263bbf1c9e30cca70d3e0aa0afa5327bfdd5d7
jodycboyd-create <jodycboyd@gmail.com>
2025-12-31 14:21:52 -0330

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -2,14 +2,9 @@
   "manifest_version": 3,
   "name": "[weong-route] + [weong-bulletin] L3",
   "version": "1.3.0",
-  "description": "Integrated Velocity, Route, and ECCC Weather Engine for NL",
-  "permissions": [
-    "storage",
-    "activeTab"
-  ],
   "content_scripts": [
     {
-      "matches": ["*://*.your-map-provider.com/*"], 
+      "matches": ["<all_urls>"],
       "js": [
         "route-engine.js",
         "velocity-widget.js",
